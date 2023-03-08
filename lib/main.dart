7f762fc86cb78e5af048e9b8b974cad3f9c04011
Isaac Chan <isaactinghong@gmail.com>
2023-03-08 14:54:59 +0800

    rename

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Chat App',
+      title: 'ChatGPT Flutter',
       theme: theme,
       home: Scaffold(
         appBar: AppBar(
