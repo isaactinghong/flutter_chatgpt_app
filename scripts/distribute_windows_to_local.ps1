f6c7724ab10268251b5ba02e0ceb0ae5fa94d647
Isaac Chan <isaactinghong@gmail.com>
2023-07-24 14:01:14 +0800

    🚀feat(scripts) Add support for *.exe files 🛠️refactor(scripts) Change distribution folder and install location ⚙️chore(scripts) Add innosetup installer creation 🛠️refactor(scripts) Modify FlutterGPTSetup.exe destination path 🚀feat(scripts) Add new innosetup.iss script 🛠️refactor(runner) Comment out ForceRedraw in flutter_window.cpp

--- scripts/distribute_windows_to_local.ps1
+++ scripts/distribute_windows_to_local.ps1
@@ -4,8 +4,16 @@ Set-Location -Path ".."
 
 
 # Flutter build into windows
-flutter build windows --release
+# flutter build windows --release
 
 # !!! This will install the Windows app on the local machine
 # copy the build output into C:/ChatGPT Flutter/
-Copy-Item -Path "build\windows\runner\Release\*" -Destination "C:\ChatGPT Flutter" -Recurse -Force
+#Copy-Item -Path "build\windows\runner\Release\*" -Destination "C:\ChatGPT Flutter" -Recurse -Force
+
+# use innosetup to create an installer
+# the installer path:  C:\Program Files (x86)\Inno Setup 6\ISCC.exe
+# run the innosetup script
+& "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" "scripts\innosetup.iss"
+
+# run the installer in scripts/output folder
+& "scripts\output\FlutterGPTSetup.exe"
